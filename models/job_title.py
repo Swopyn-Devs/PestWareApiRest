0352c5fc6c99f545afec27285e97dcf8226a1cdc
Ricardo Castro <onericky16@gmail.com>
2022-03-31 15:27:58 -0600

    add oauth2_schema & onupdate to model field

--- models/job_title.py
+++ models/job_title.py
@@ -13,5 +13,5 @@ class JobTitle(Base):
     name = Column(String, nullable=False)
     job_center_id = Column(GUID, nullable=False)
     is_deleted = Column(Boolean, default=False)
-    created_at = Column(DateTime, onupdate=datetime.datetime.now)
-    updated_at = Column(DateTime, server_default=func.now())
+    created_at = Column(DateTime, default=datetime.datetime.now())
+    updated_at = Column(DateTime, onupdate=datetime.datetime.now())
--- routers/job_titles.py
+++ routers/job_titles.py
@@ -7,6 +7,7 @@ from sqlalchemy.orm import Session
 from database import get_db
 from repository import job_title
 from schemas.job_title import JobTitleRequest, JobTitleResponse
+from utils.jwt import oauth2_schema
 
 router = APIRouter(
     prefix='/job_titles',
@@ -15,25 +16,25 @@ router = APIRouter(
 
 
 @router.get('', status_code=status.HTTP_200_OK, response_model=List[JobTitleResponse])
-async def index(db: Session = Depends(get_db)):
+async def index(db: Session = Depends(get_db), token: str = Depends(oauth2_schema)):
     return job_title.get_all(db)
 
 
 @router.get('/{job_title_id}', status_code=status.HTTP_200_OK, response_model=JobTitleResponse)
-async def show(job_title_id: UUID4, db: Session = Depends(get_db)):
+async def show(job_title_id: UUID4, db: Session = Depends(get_db), token: str = Depends(oauth2_schema)):
     return job_title.retrieve(db, job_title_id)
 
 
 @router.post('', status_code=status.HTTP_201_CREATED, response_model=JobTitleResponse)
-async def store(request: JobTitleRequest, db: Session = Depends(get_db)):
+async def store(request: JobTitleRequest, db: Session = Depends(get_db), token: str = Depends(oauth2_schema)):
     return job_title.create(db, request)
 
 
 @router.put('/{job_title_id}', status_code=status.HTTP_202_ACCEPTED, response_model=JobTitleResponse)
-async def update(job_title_id: UUID4, request: JobTitleRequest, db: Session = Depends(get_db)):
+async def update(job_title_id: UUID4, request: JobTitleRequest, db: Session = Depends(get_db), token: str = Depends(oauth2_schema)):
     return job_title.update(db, request, job_title_id)
 
 
 @router.delete('/{job_title_id}', status_code=status.HTTP_204_NO_CONTENT)
-async def destroy(job_title_id: UUID4, db: Session = Depends(get_db)):
+async def destroy(job_title_id: UUID4, db: Session = Depends(get_db), token: str = Depends(oauth2_schema)):
     return job_title.delete(db, job_title_id)
