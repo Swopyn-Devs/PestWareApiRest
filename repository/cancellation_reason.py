2b9b13077626e076166b682dff9e3c599144014d
Ricardo Castro <rcastro@zegucom.com.mx>
2022-04-18 17:09:39 -0500

    add plagues and cancellation resasons catalogs

---
+++ repository/cancellation_reason.py
@@ -0,0 +1,36 @@
+from utils.functions import *
+
+from pydantic import UUID4
+from sqlalchemy.orm import Session
+from fastapi_jwt_auth import AuthJWT
+
+from models.cancellation_reason import CancellationReason
+from schemas.cancellation_reason import CancellationReasonRequest, CancellationReasonUpdateRequest
+
+model_name = 'motivo de cancelación'
+
+
+def get_all(db: Session, authorize: AuthJWT):
+    return get_all_data(db, CancellationReason, authorize)
+
+
+def retrieve(db: Session, model_id: UUID4):
+    return get_data(db, CancellationReason, model_id, model_name)
+
+
+def create(db: Session, request: CancellationReasonRequest, authorize: AuthJWT):
+    employee = get_employee_id_by_token(db, authorize)
+    request_data = CancellationReason(
+        name=request.name,
+        job_center_id=employee.job_center_id,
+    )
+    insert_data(db, request_data)
+    return request_data
+
+
+def update(db: Session, request: CancellationReasonUpdateRequest, model_id: UUID4):
+    return update_data(db, CancellationReason, model_id, model_name, request.dict())
+
+
+def delete(db: Session, model_id: UUID4):
+    return update_delete(db, CancellationReason, model_id, model_name)
---
+++ routers/cancellation_reasons.py
@@ -0,0 +1,44 @@
+from fastapi import APIRouter, Depends, status
+from fastapi_jwt_auth import AuthJWT
+from fastapi_pagination import Page
+from pydantic import UUID4
+from sqlalchemy.orm import Session
+
+from database import get_db
+from repository import cancellation_reason
+from schemas.cancellation_reason import CancellationReasonRequest, CancellationReasonUpdateRequest, CancellationReasonResponse
+
+router = APIRouter(
+    prefix='/cancellation-reasons',
+    tags=['🚫 Motivos de cancelación']
+)
+
+
+@router.get('', status_code=status.HTTP_200_OK, response_model=Page[CancellationReasonResponse])
+async def index(db: Session = Depends(get_db), authorize: AuthJWT = Depends()):
+    authorize.jwt_required()
+    return cancellation_reason.get_all(db, authorize)
+
+
+@router.get('/{cancellation_reason_id}', status_code=status.HTTP_200_OK, response_model=CancellationReasonResponse)
+async def show(cancellation_reason_id: UUID4, db: Session = Depends(get_db), authorize: AuthJWT = Depends()):
+    authorize.jwt_required()
+    return cancellation_reason.retrieve(db, cancellation_reason_id)
+
+
+@router.post('', status_code=status.HTTP_201_CREATED, response_model=CancellationReasonResponse)
+async def store(request: CancellationReasonRequest, db: Session = Depends(get_db), authorize: AuthJWT = Depends()):
+    authorize.jwt_required()
+    return cancellation_reason.create(db, request, authorize)
+
+
+@router.put('/{cancellation_reason_id}', status_code=status.HTTP_202_ACCEPTED, response_model=CancellationReasonResponse)
+async def update(cancellation_reason_id: UUID4, request: CancellationReasonUpdateRequest, db: Session = Depends(get_db), authorize: AuthJWT = Depends()):
+    authorize.jwt_required()
+    return cancellation_reason.update(db, request, cancellation_reason_id)
+
+
+@router.delete('/{cancellation_reason_id}', status_code=status.HTTP_204_NO_CONTENT)
+async def destroy(cancellation_reason_id: UUID4, db: Session = Depends(get_db), authorize: AuthJWT = Depends()):
+    authorize.jwt_required()
+    return cancellation_reason.delete(db, cancellation_reason_id)
